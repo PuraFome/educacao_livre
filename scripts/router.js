68cc9cdac011faee9600cecff80d6378e27979dc
Pura Fome <samuelpe06062000@gmail.com>
2024-11-22 22:38:47 -0300

    fix: deploy on github pages with hash-based routing

--- scripts/router.js
+++ scripts/router.js
@@ -6,15 +6,15 @@ function loadPage(page) {
         })
         .catch(error => {
             console.error('Erro ao carregar a página:', error);
-            document.gerElementById('content').innerHTML = '<p>Erro ao carregar a página. Tente novamente mais tarde.</p>';
+            document.getElementById('content').innerHTML = '<p>Erro ao carregar a página. Tente novamente mais tarde.</p>';
         });
 }
 
-page.base('/educacao_livre');
+page.base('/educacao_livre'); // Define a base para o roteamento no GitHub Pages
 page('/', () => loadPage('home'));
 page('/tutor_register', () => loadPage('tutor_register'));
 page('/available_classes', () => loadPage('available_classes'));
 
 page({
-    hashbang: true
+    hashbang: true // Usa hash-based routing
 });
