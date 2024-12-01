bf9b7be2ab18274578016b13ee14166fa066ea5b
Pura Fome <samuelpe06062000@gmail.com>
2024-12-01 15:48:46 -0300

    feat: add logo

--- scripts/menu.js
+++ scripts/menu.js
@@ -3,11 +3,13 @@ document.addEventListener('DOMContentLoaded', () => {
     const navLinks = document.querySelector('.nav-links');
     const header = document.querySelector('header');
     const main = document.querySelector('main');
+    const logo = document.querySelector('.logo');
 
     const toggleMenu = () => {
         navLinks.classList.toggle('expanded');
         header.classList.toggle('expanded');
         main.classList.toggle('shifted');
+        logo.classList.toggle('expanded');
     };
 
     menuToggle.addEventListener('click', toggleMenu);
